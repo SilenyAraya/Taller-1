ef5934eee7d38990049272cd61e01952f33e7750
Usuario <silenyaraya97gmail.com>
2017-08-03 11:39:53 -0600

    final

--- src/laboratorios/Laboratorios.java
+++ src/laboratorios/Laboratorios.java
@@ -21,7 +21,7 @@ public class Laboratorios {
                 agregarNombres();
                 break;
             case 2:
-                
+                libros();
                 break;
             case 3:
                 break;
@@ -33,26 +33,47 @@ public class Laboratorios {
     }   
 }
     public static void agregarNombres() {
-        String matriz[][] = new String[3][3];
-        for (int i = 0; i < matriz.length; i++) {
-            for (int x = 0; x < matriz.length; x++) {
-              System.out.println("Ingrese el nombre de la persona:");
-              int nombre = ingresar.nextInt();
-
-              } 
-            }
-            for (int y = 0; y < matriz[x].length; y++) {
-                    System.out.println(matriz[x][y]);
-                }
-
-            
-       
-
+        Scanner nombre = new Scanner(System.in);
+        String matrizNombre[][] = new String[3][3];
+        for(int j=0; j<3; j++){
+            for(int i=0; i<3; i++){
+                System.out.print("Introduzca el nombre ("+j+", "+i+"): ");
+                matrizNombre[j][i]=nombre.next();
+              
+            }    
+        }
+        //System.out.print(matrizNombre[j][i]+" ");
     }
+   
+    public static void libros() {
+        System.out.println("\n1.Agregar libro\n2Libros Vendidos\n3 Libros Alquilados\n4.Salir\n\n"
+                + "Seleccione una opción: ");
+        int opcionLibro = ingresar.nextInt();
+        switch (opcionLibro) {
+            case 1:
+                List listaLibros = new ArrayList();
+             System.out.println("Escriba el nombre del libro");
+             String libro= ingresar.next();
+             listaLibros.add(libro);
+             System.out.println("El libro fue agregado correctamente");
+             System.out.println(listaLibros);
+                break;
+            case 2:
+              
+                break;
+            case 3:
+                
+                break;
+            case 4:
+                menuPrincipal();
+                break;
+    }
+        }
 
     public static void main(String[] args) {
         menuPrincipal();
         agregarNombres();
+        libros();
     }
 }
 
